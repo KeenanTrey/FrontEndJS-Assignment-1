d6f995bbcf845c69f1e1a36dd51fb62c14c704ec
KeenanTrey <46493050+KeenanTrey@users.noreply.github.com>
2021-02-06 15:48:34 -0700

    Adding assignment

---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+# Keenan Philip Assignment 1
+
+This assignment will take a  simple search from user input and if the stock symbol searched for is valid, The API will fetch the relevant data and will be displayed back to the user in an appended list. If no valid data is returned the template will inform the user the stock could not be found.
---
+++ js/index.js
@@ -0,0 +1,52 @@
+window.addEventListener("load", function(e) {
+    const baseURL = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&apikey=6OW78JBH84PRLQDP&symbol='
+
+    
+
+    async function getData(symbol)
+    {
+        let url = baseURL + symbol
+        const req = await fetch(url)
+        const res = await req.json()
+        return res
+    }
+
+     document.forms.search.addEventListener('submit', function(e) {
+         e.preventDefault()
+         const res = getData(e.currentTarget.elements.searchTerm.value)
+         
+
+         res.then(res => {
+             
+             console.log(Object.values(res['Time Series (Daily)'])[0])
+             //pull out data for list
+             let data = Object.values(res['Time Series (Daily)'])[0]
+             console.log(data['1. open'])
+             if(res['Error Message'])
+             {
+                const errTemplate =   `<p>Can't find stock quote</p>`
+                document.querySelector('.results').innerHTML = errTemplate
+             }
+             else
+             {
+                const successTemplate =   `
+                <ul>
+                    <li>${res['Meta Data']['2. Symbol']}</li>
+                    <li>Date: ${res['Meta Data']['3. Last Refreshed']}</li>
+                    <li>High Price: <span>$${data['1. open']}</span></li>
+                    <li>Low Price: <span>$${data['3. low']}</span></li>
+                    <li>Closing Price: <span>$${data['4. close']}</span></li>
+                    <li>Split Coefficient: <span>$${data['8. split coefficient']}</span></li>
+                </ul>`
+                document.querySelector('.results').innerHTML = successTemplate
+
+                console.log(successTemplate)
+
+                
+             }
+         })
+     })
+    
+
+    
+})
